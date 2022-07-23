3876631fd2e97e0055b3d598926f74891badc5c8
Milena <milenaferreira294@hotmail.com>
2022-07-22 23:49:03 -0300

    Add example tile
    Create scriptable object for visual tile for testing.

---
+++ Assets/Scripts/EmptyTile.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptablesObjects/Tiles/EmptyTile")]
+public class EmptyTile : TileTypeBase
+{
+
+}
