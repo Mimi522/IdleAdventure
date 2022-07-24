d21fe95a66589613ebd51f9a93de371e9c7dd54a
Milena <milenaferreira294@hotmail.com>
2022-07-23 23:34:12 -0300

    Rename scriptableobject class

--- Assets/Scripts/TileBase.cs
+++ Assets/Scripts/TileBase.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TileTypeBase : ScriptableObject
+public class TileBase : ScriptableObject
 {
     public string DisplayName;
 
