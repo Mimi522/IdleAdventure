1941ffab054a58380e6415d92f496e8549845c8c
Milena <milenaferreira294@hotmail.com>
2022-07-23 23:30:26 -0300

    Rename scriptableobject class

---
+++ Assets/Scripts/DefaultTileFactory.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptablesObjects/Tiles/DefaultTileFactory")]
+public class DefaultTileFactory : TileBase
+{
+}
