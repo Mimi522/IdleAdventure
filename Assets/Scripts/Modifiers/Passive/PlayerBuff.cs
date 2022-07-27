160da71285c237847e440d87f354121163a868cd
Milena <milenaferreira294@hotmail.com>
2022-07-27 14:45:32 -0300

    Create passive buff

---
+++ Assets/Scripts/Modifiers/Passive/PlayerBuff.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptablesObjects/Objects/Player Buff")]
+public class PlayerBuff : TileModifier
+{
+    public int Flat;
+    public int Percentage;
+
+    private BattleManager _battleManager;
+
+    public override void OnPlaced()
+    {
+        _battleManager = BattleManager.Instance;
+        _battleManager.Player.DamageDealtBonus.Percentage += Percentage;
+        _battleManager.Player.DamageDealtBonus.Flat += Flat;
+    }
+}
