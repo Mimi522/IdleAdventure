f3d030d3f30e372223efc7a50e507f80c94460d9
Milena <milenaferreira294@hotmail.com>
2022-07-28 03:58:26 -0300

    Organize code

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -5,36 +5,28 @@ using UnityEngine;
 /// </summary>
 public class Tile : MonoBehaviour
 {
+    public TileModifier ModifierData { get; private set; }
+
+    public TileType TileType { private get; set; } = TileType.Passive;
+
     private GameObject _modifierVisual;
 
-    private TileModifier _modifierData;
-    public TileModifier ModifierData {
-        get { return _modifierData; }
-        set { _modifierData = value; }
-    }
-
-    private TileType _tileType = TileType.Passive;
-    public TileType TileType {
-        set { _tileType = value; }
-    }
-
-    // Placing an object in a tile only if the tile is empty
     public bool TryApplyModifier(TileModifier objectPrefab)
     {
-        if (_modifierData != null) {
+        if (ModifierData != null) {
             return false;
         }
 
-        if (objectPrefab.ValidTarget != _tileType) {
+        if (objectPrefab.ValidTarget != TileType) {
             return false;
         }
 
         Vector3 objectPosition = transform.position;
 
-        _modifierData = objectPrefab;
+        ModifierData = objectPrefab;
         _modifierVisual = GameObject.Instantiate(objectPrefab.VisualPrefab, objectPosition, Quaternion.identity);
 
-        _modifierData.OnPlaced();
+        ModifierData.OnPlaced();
 
         return true;
     }
